0ab15df11f47aa106357e8dbdbf47bb6b1eea7d9
Arpit <arpitdeosthale12@gmail.com>
2025-09-22 01:46:03 +0530

    yt

--- 101/userinput.c
+++ 101/userinput.c
@@ -5,11 +5,11 @@ int main(){
     int j;  // If not defined it will take a random varible called garbage value
     int i;
     int k;
-    printf("Enter 2 values");
+    printf("Enter 2 values \n");
     scanf("%d" , &i);    // The "&"" sign gives us the address of the variable
     scanf("%d" , &j);
     k = i + j;
-    printf("The Value of addition of %d and %d is %d" , j ,i ,k );  // This is callled as print format
+    printf("The Value of addition of %d and %d is \n %d" , j ,i ,k );  // This is callled as print format
     return 0;
 
 }
