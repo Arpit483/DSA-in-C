4df5f8597937718ac6b569d3bd786c47f7203831
Arpit <arpitdeosthale12@gmail.com>
2025-09-25 01:24:22 +0530

    Done

--- 101/pointer.c
+++ 101/pointer.c
@@ -2,12 +2,12 @@
 
 int main (){
 // Pointers
-    // int i = 5;
-    // int *p;
-    // p = &i;
-    // printf("%d \n" , i);
-    // printf("%d \n" , p);
-    // printf("%d \n" , *p);
+    int i = 5;
+    int *p;
+    p = &i;
+    printf("%d \n" , i);
+    printf("%d \n" , p);
+    printf("%d \n" , *p);
     
 // Pointers in Array
     int values[] = {5,2,3,4,7};
